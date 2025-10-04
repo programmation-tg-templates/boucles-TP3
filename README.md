ca00d7e8ce3a6606a4ecfae4c5cb525de123ae57
François Roland <francois.roland@heh.be>
2025-10-04 16:53:52 +0200

    Write unit tests.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# base
+# Boucles - TP3 - La demi-pyramide
 
-Consignes
+## Consignes
+
+Écrire une fonction qui permet de construire une chaîne de caractère représentant une demi-pyramide de `n` étages.
+
+Par exemple, si `n` vaut `3`, la fonction retourne la chaîne `*\n**\n***\n`.
+
+Si vous affichez cette ligne en console, vous obtenez la demi-pyramide :
+```
+*
+**
+***
+```
+Le nombre `n` est le paramètre de la fonction `boucle3`.
--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,9 +1,17 @@
 // Tests unitaires écrits par l'enseignant. Ne modifiez pas ce fichier.
 
-import {...} from '../index';
+import {boucle3} from '../index';
 
-describe('...', () => {
-    test('...', () => {
-        expect(false).toBe(true);
+describe('La fonction boucle 3 doit retourner', () => {
+    test('une demi-pyramide à 1 étage', () => {
+        expect(boucle3(1)).toBe('*\n');
+    });
+
+    test('une demi-pyramide à 3 étages', () => {
+        expect(boucle3(3)).toBe('*\n**\n***\n');
+    });
+
+    test('une demi-pyramide à 5 étages', () => {
+        expect(boucle3(5)).toBe('*\n**\n***\n****\n*****\n');
     });
 });
